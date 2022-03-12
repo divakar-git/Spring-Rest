ce18012398ae5c33aa6cd4a933e38c2c2abfe533
Divakar <divakarrc1991@gmail.com>
2022-03-11 21:03:48 -0500

    Initial Import by TL

---
+++ Tools/GITTLWS/DemoProj01/src/main/java/com/nt/DemoProj01Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProj01Application.class, args);
+	}
+
+}
