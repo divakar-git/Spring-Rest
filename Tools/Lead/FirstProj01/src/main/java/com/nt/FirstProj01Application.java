61f4f303f1e3c3f83cff927c86d3decbd710e24d
divakar <divakarrc1991@gmail.com>
2022-03-11 21:44:17 -0500

    Initial Commit by Lead

---
+++ Tools/Lead/FirstProj01/src/main/java/com/nt/FirstProj01Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProj01Application.class, args);
+	}
+
+}
